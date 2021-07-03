60da56f2c627f57835fc6c023e5dc465466d76d8
matsuda <matsuda12357@gmail.com>
2021-07-03 22:12:02 +0900

    gridに変更

--- app.js
+++ app.js
@@ -3,28 +3,22 @@ const parts1=document.getElementById('my-parts1') //左上
 const parts2=document.getElementById('my-parts2') //右上
 const parts3=document.getElementById('my-parts3') //左下
 const parts4=document.getElementById('my-parts4') //右下
+const grid=document.getElementById('grid')
 const partsArray=[parts1,parts2,parts3,parts4]
 let firstDegree //クリック時の角度
 let itemCenter={}　//アイテムの中心座標
 let nowDegree=0　//現在の角度
 let degree　//現在動かしている角度
+const firstPosition=item.getBoundingClientRect() //itemの最初の座標
 item.onclick=(e)=>{
     const size=e.target.getBoundingClientRect()
     itemCenter={
         x:size.left+size.width/2,
         y:size.top+size.height/2
     }
-    parts1.style.top=`${size.top-100}px`
-    parts1.style.left=`${size.left-100}px`
 
-    parts2.style.top=`${size.top-100}px`
-    parts2.style.left=`${size.right}px`
-
-    parts3.style.top=`${size.bottom}px`
-    parts3.style.left=`${size.left-100}px`
-
-    parts4.style.top=`${size.bottom}px`
-    parts4.style.left=`${size.right}px`
+    grid.style.top=`${firstPosition.top-100}px` //-100はgridの1列目のheight
+    grid.style.left=`${firstPosition.left-100}px` //-100はgridの1行目のwidth
 }
 
 partsArray.forEach(parts=>{
@@ -48,16 +42,11 @@ const mouseMove=(e)=>{
     const size=e.target.getBoundingClientRect()
     degree = Math.atan2(e.pageX - itemCenter.x, - (e.pageY - itemCenter.y) )*(180/Math.PI); 
     item.style.transform=`rotate(${nowDegree+(degree-firstDegree)}deg)`
-    parts1.style.transform=`rotate(${nowDegree+(degree-firstDegree)}deg)`
-    parts2.style.transform=`rotate(${nowDegree+(degree-firstDegree)}deg)`
-    parts3.style.transform=`rotate(${nowDegree+(degree-firstDegree)}deg)`
-    parts4.style.transform=`rotate(${nowDegree+(degree-firstDegree)}deg)`
-    setPartsStyle(item.getBoundingClientRect())
+    grid.style.transform=`rotate(${nowDegree+(degree-firstDegree)}deg)`
 }
 
 
 const setPartsStyle=(size)=>{
-    console.log(size)
     parts1.style.top=`${size.top-100}px`
     parts1.style.left=`${size.left-100}px`
 
@@ -73,15 +62,9 @@ const setPartsStyle=(size)=>{
 document.onclick=(e)=>{
     if(e.target.closest('#item')) {
         item.classList.add('border')
-        parts1.classList.remove('none')
-        parts2.classList.remove('none')
-        parts3.classList.remove('none')
-        parts4.classList.remove('none')
+        grid.classList.remove('none')
     }else{
         item.classList.remove('border')
-        parts1.classList.add('none')
-        parts2.classList.add('none')
-        parts3.classList.add('none')
-        parts4.classList.add('none')
+        grid.classList.add('none')
     }
 }
